88ac87e1419d47433c6b5cc67008a00b51903f5b
teruma-nanami <kinos.travel.fun@gmail.com>
2024-06-25 20:08:15 +0900

    make-front

---
+++ src/resources/views/attendance.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/attendance.css')}}">
+@endsection
+
+@section('content')
+<h2>2021-11-01</h2>
+<table>
+  <tbody>
+    <tr>
+      <th>名前</th>
+      <th>勤務開始</th>
+      <th>勤務終了</th>
+      <th>休憩時間</th>
+      <th>勤務時間</th>
+    </tr>
+    <tr>
+      <td>テスト太郎</td>
+      <td>10:00:00</td>
+      <td>10:00:00</td>
+      <td>10:00:00</td>
+      <td>10:00:00</td>
+    </tr>
+    <tr>
+      <td>テスト太郎</td>
+      <td>10:00:00</td>
+      <td>10:00:00</td>
+      <td>10:00:00</td>
+      <td>10:00:00</td>
+    </tr>
+    <tr>
+      <td>テスト太郎</td>
+      <td>10:00:00</td>
+      <td>10:00:00</td>
+      <td>10:00:00</td>
+      <td>10:00:00</td>
+    </tr>
+    <tr>
+      <td>テスト太郎</td>
+      <td>10:00:00</td>
+      <td>10:00:00</td>
+      <td>10:00:00</td>
+      <td>10:00:00</td>
+    </tr>
+    <tr>
+      <td>テスト太郎</td>
+      <td>10:00:00</td>
+      <td>10:00:00</td>
+      <td>10:00:00</td>
+      <td>10:00:00</td>
+    </tr>
+  </tbody>
+</table>
+<div class="page__nav">
+  </div>
+@endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+// Route::get('/', [TimestampController::class, 'index']);
+
+Route::get('/', function () {  return view('attendance');
 });
